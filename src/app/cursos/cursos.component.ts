9ecab0bfe88dd8da4bcc4abedb46efe9e052a3c4
Marcelo Rebouças <marcelo_reboucas@hotmail.com>
2019-04-15 23:42:45 -0300

    Implementaçao da estrutura de service do curso

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -7,7 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal:string;
+  cursos:string[];
+
+  constructor(private cursosService: CursosService) { 
+
+    this.nomePortal = "https://loiane.training/";
+    this.cursos = ['Java', 'Ext Js','Angular'];
+    
+    //var service = new CursosService();
+
+    this.cursos = this.cursosService.getCursos();
+
+  }
+
 
   ngOnInit() {
   }
