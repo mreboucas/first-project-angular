e9fbd79508e9481954cf0cbeeac491789708c4dd
Marcelo Rebouças <marcelosreboucas@gmail.com>
2019-08-06 13:25:52 -0300

    data-binding uploaded

---
+++ src/app/data-binding/data-binding.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.sass']
+})
+export class DataBindingComponent implements OnInit {
+  /**
+   * declare :any to accedpt anything
+   */
+  url:string = 'http://loaine.com'
+
+  urlImage:string = "https://picsum.photos/200/300?grayscale"
+
+  cursoAngular = true
+/**
+ * ES 2015 não precisa declarecer a função com o nome function, pode ser omitido. E tb dispensa ;
+ */
+  getValor() {
+    return 2;
+  }
+
+  getCurtirCurso() {
+    return true;
+  }
+
+  constructor() { 
+
+  }
+
+  ngOnInit() {
+  }
+
+}
